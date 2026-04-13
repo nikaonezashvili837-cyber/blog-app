02df2401fdcbdbf12f1d8bc8d18bc9526b30131e
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-04-13 21:56:28 +0400

    Setting up basic server

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { IncomingMessage, ServerResponse } from "node:http";
+import http from 'http'
+const server:http.Server  = http.createServer((req:IncomingMessage,res:ServerResponse)=>{
+    res.end('hello world')
+});
+server.listen(3000);
