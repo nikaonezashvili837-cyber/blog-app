6d5f5b7742d0ce236a21f362f261fc05e033a0fd
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-04-13 22:36:52 +0400

    Feat:building feature which will let user to display all data when hitting /posts endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { IncomingMessage, ServerResponse } from "node:http";
-import http from 'http'
+import http from 'http';
+import { readFileSync } from "node:fs";
+const data = readFileSync(`${process.cwd()}/data/data.json`,'utf-8');
 const server:http.Server  = http.createServer((req:IncomingMessage,res:ServerResponse)=>{
-    res.end('hello world')
+    const url:string | undefined = req.url;
+    const method:string | undefined = req.method;
+    if(url === '/posts' && method === 'GET'){
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(data)
+    }
 });
 
 server.listen(3000);
