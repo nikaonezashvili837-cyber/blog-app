aa74821acbecbbce3575b112600091844558652d
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-04-14 21:33:41 +0400

    Feat:implementing feature which let's user to find individual post by id

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 import { IncomingMessage, ServerResponse } from "node:http";
 import http from 'http';
 import { readFileSync } from "node:fs";
-const data = readFileSync(`${process.cwd()}/data/data.json`,'utf-8');
+const data = JSON.parse(readFileSync(`${process.cwd()}/data/data.json`,'utf-8'));
+function findPost(id:number){
+    return data.find(post=> post.id === id)
+}
 const server:http.Server  = http.createServer((req:IncomingMessage,res:ServerResponse)=>{
     const url:string | undefined = req.url;
     const method:string | undefined = req.method;
+    let id:string | undefined;
+    if(url) {
+       id =  url.split('/')[2];
+    }
+    const post = findPost(Number(id));
+    res.writeHead(200,{'Content-Type':'application/json'});
     if(url === '/posts' && method === 'GET'){
-        res.writeHead(200,{'Content-Type':'application/json'});
-        res.end(data)
+        return res.end(JSON.stringify({
+            status:'ok',
+            data
+        }));
+    }else if(post && method === 'GET'){
+        return res.end(JSON.stringify({
+            status:'ok',
+            data:post
+        }));
+    }else if(Number(id) > data.length -1 ){
+        return res.end(JSON.stringify({
+            status:'404',
+            data:'No data'
+        }))
     }
 });
 
